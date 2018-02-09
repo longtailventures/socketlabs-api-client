986f35a530df07b1257d4d780552c35713f2a122
longtailventures <mnavasca@lotlinx.com>
2018-02-09 12:42:20 -0600

    Update SocketLabsApiClient.php

--- src/SocketLabsApiClient.php
+++ src/SocketLabsApiClient.php
@@ -6,72 +6,157 @@ use stdClass;
 
 class SocketLabsApiClient
 {
-    const ACCOUNT_ID = '';
-    const API_KEY = '';
-    const SERVER_ID = '';
+    private $_apiKey, $_serverId;
+    private $_messageData;
 
     const API_URL = 'https://inject.socketlabs.com/api/v1/email';
 
 
-    public function __construct($debug = false)
+    /**
+     * constructor
+     *
+     * @param string $socketLabsApiKey
+     * @param int $socketLabsServerId
+     * @param string $debug
+     */
+    public function __construct($socketLabsApiKey, $socketLabsServerId)
     {
-        $this->_debug = $debug;
+        $this->_apiKey = $socketLabsApiKey;
+        $this->_serverId = $socketLabsServerId;
+
+        $this->_messageData = [
+            'Subject' => '',
+            'TextBody' => '',
+            'HtmlBody' => '',
+            'To' => '',
+            'From' => '',
+            'ReplyTo' => '',
+        ];
     }
 
 
-    public function sendMail(array $mailTo, array $mailFrom, $subject, array $messageTexts, array $attachments = [])
+    /**
+     * set subject of email
+     */
+    public function setSubject($subject)
+    {
+        $this->_messagesData['Subject'] = $subject;
+    }
+
+
+    /**
+     * set Html body of email message
+     *
+     * @param string $body
+     */
+    public function setHtmlBody($body)
+    {
+        $this->_messageData['HtmlBody'] = $body;
+    }
+
+
+    /**
+     * set Text body of email message
+     *
+     * @param string $body
+     */
+    public function setTextBody($body)
+    {
+        $this->_messageData['TextBody'] = $body;
+    }
+
+
+    /**
+     * set from address for email
+     *
+     * @param string $name
+     * @param string $email
+     */
+    public function setFromAddress($name, $email)
+    {
+        $this->_messageData['From'] = [
+            'EmailAddress' => $email,
+            'FriendlyName' => $name
+        ];
+
+        // set replyto to incoming as default
+        $this->_messageData['ReplyTo'] = $this->_messageData['From'];
+    }
+
+
+    /**
+     * set reply to address for email
+     *
+     * @param string $name
+     * @param string $email
+     */
+    public function setReplyToAddress($name, $email)
+    {
+        $this->_messageData['ReplyTo'] = [
+            'EmailAddress' => $email,
+            'FriendlyName' => $name
+        ];
+
+    }
+
+
+    /**
+     * set to address for email
+     *
+     * @param string $name
+     * @param string $email
+     */
+    public function setToAddress($name, $email)
+    {
+        $this->_messageData['To'] = [
+            'EmailAddress' => $email,
+            'FriendlyName' => $name
+        ];
+    }
+
+
+    /**
+     * add cc address for email
+     *
+     * @param string $name
+     * @param string $email
+     */
+    public function addCcAddress($name, $email)
+    {
+        $this->_messageData['Cc'] = [
+            'EmailAddress' => $email,
+            'FriendlyName' => $name
+        ];
+    }
+
+
+    /**
+     * add attachment to email
+     *
+     * @param string $attachmentFileName
+     * @param string $fileName
+     */
+    public function addAttachment($attachmentFileName, $fileName)
+    {
+        if (!isset($this->_messageData['Attachments']))
+            $this->_messageData['Attachments'] = [];
+        $this->_messageData['Attachments'] = $fileName;
+    }
+
+
+    /**
+     * send email
+     *
+     * @return boolean $isSent
+     * true if successful, false otherwise
+     */
+    public function send()
     {
         // re: https://github.com/socketlabs/email-on-demand-examples/blob/master/PHP/Injection%20API/http_injection_merge.php
         $data = new stdClass();
-        $data->ServerId = self::SERVER_ID;
-        $data->ApiKey = self::API_KEY;
-
-        if ($this->_debug)
-        {
-            $mailTo = [];
-            $mailTo[] = [
-                'email' => $this->_debugEmail,
-                'name' => 'LotLinx Support'
-            ];
-        }
-
-        $messagesData = array(
-            'Subject' => $subject,
-            'TextBody' => $messageTexts['text'],
-            'To' => array(array(
-                'EmailAddress' => $mailTo[0]['email'],
-                'FriendyName' => $mailTo[0]['name']
-            )),
-            'From' => array(
-                'EmailAddress' => $mailFrom['email'],
-                'FriendlyName' => $mailFrom['name']
-            ),
-            'ReplyTo' => array(
-                'EmailAddress' => $mailFrom['email'],
-                'FriendlyName' => $mailFrom['name']
-            )
-        );
-
-        if (isset($messageTexts['html']))
-            $messagesData['HtmlBody'] = $messageTexts['html'];
-
-        if (count($mailTo) > 1)
-        {
-            $messagesData['Cc'] = [];
-
-            for ($i = 1; $i < count($mailTo); $i++)
-            {
-                $messagesData['Cc'][] = [
-                    'EmailAddress' => $mailTo[$i]['email'],
-                    'FriendlyName' => $mailTo[$i]['name']
-                ];
-            }
-        }
-
-        if (count($attachments) > 0)
-            $messagesData['Attachments'] = $attachments;
-
-        $data->Messages = array($messagesData);
+        $data->ServerId = $this->_serverId;
+        $data->ApiKey = $this->_apiKey;
+        $data->Messages = array($this->_messageData);
 
         $emailData = json_encode($data);
 
